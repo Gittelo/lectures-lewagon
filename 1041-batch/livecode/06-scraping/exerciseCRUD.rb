556c80719d21a37454c7af6e580f7d42dc114709
diogo telo <diogo_telo@hotmail.com>
2022-10-18 18:15:04 +0100

    extra CRUD

---
+++ 1041-batch/livecode/06-scraping/exerciseCRUD.rb
@@ -0,0 +1,16 @@
+items = [["gameboy", false], ["nintendo",true]]
+
+items2 = [{item_name: "gameboy", status: false}, {item_name: "nitendo", status: tru}]
+
+items3 = {gameboy: {status: false, price: 3}, nitendo: {status: true, price: 34}}
+
+
+# print the value of the status for nintendo (items, items2)
+# print the price of the nintendo
+
+# create a new record for each one of them with "ps5" and status. true (items3 random price)
+
+# update the gameboy for items and items2 to ps3
+# update the gameboy price for 100 in items 3
+
+# delete the second record for items, items 2, items 3
